2ade18ca4250af2a131b03aee6d6bc854d59aac7
DR-84 <58080204+DR-84@users.noreply.github.com>
2020-05-17 17:58:46 +0200

    last excercise finished

--- supermarket_rapport.py
+++ supermarket_rapport.py
@@ -1,6 +1,7 @@
 # initial information
 # open from 8:00 till 22:00
-# discounts: -33% & -50% if almost past exp.date -35%  (exp discount can go on top of regular discount!)
+# discounts: -33% & -50% if almost past exp.date -35%
+# (exp discount can go on top of regular discount!)
 # start amount is init amount
 # produce is sold at end of every hour.
 # cannot sell more then available
@@ -14,7 +15,8 @@
 # order new produce if less then a certain %. order 100%?
 
 # rapports:
-# hourly: how much produce left, how much is almost* exp.date, how  much is exp.date, how much money made.
+# hourly: how much produce left, how much is almost* exp.date,
+# how  much is exp.date, how much money made.
 # *almost is relative because for something that lasts 10 days alsmost is 1 maybe 2 days for
 # something of 365 days almost is a week or two.
 # daily: per product: how much was made on this, was it on sale? what kind of sale?
