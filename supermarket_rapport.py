f867bad13b1d0257a66a133f5bd7e2a1126157e8
DR-84 <58080204+DR-84@users.noreply.github.com>
2020-05-20 10:51:27 +0200

    got stuff doing stuff (made a functioning def)

--- supermarket_rapport.py
+++ supermarket_rapport.py
@@ -1,85 +1,118 @@
-# initial information
-# open from 8:00 till 22:00
-# discounts: -33% & -50% if almost past exp.date -35%
-# (exp discount can go on top of regular discount!)
-# start amount is init amount
-# produce is sold at end of every hour.
-# cannot sell more then available
-# random  regular sell counter for every product is between 25%-75%
-# random discount sell counter for every product is between 50%-100%
-# random exp.discount sell counter also between 50%-100%
-# orders arrive next day 12 afternoon, arrived means available!
-# 2 random products on sale every 3 days, products cannot be on sale twice in a row.
-# exp. date is delivery day + days to expire
-# if exp.date is expired throw out
-# order new produce if less then a certain %. order 100%?
-
-# rapports:
-# hourly: how much produce left, how much is almost* exp.date,
-# how  much is exp.date, how much money made.
-# *almost is relative because for something that lasts 10 days alsmost is 1 maybe 2 days for
-# something of 365 days almost is a week or two.
-# daily: per product: how much was made on this, was it on sale? what kind of sale?
-# daily: how much dit we pay for the products, how much did we make on the products.
-# daily: what was ordered, how much and how much was that.
-# weekly: how much profit/loss last week.
-# weekly: how much was lost on expired produce.
-# ALL OF THE ABOVE MUST BE REPORTED IN .TXT FILES PER DAY THAT INCLUDES HOURLY LOGS,
-# IN A WEEK REPORT THAT INCLUDES DAY REPORTS AND WEEK REPORT.
+import datetime
+import supermarket_data
+import random
 
 
-products = [
-    {"name": "apple", "keeps_days": 10, "buy": 0.1, "sell": 0.7, "order_size": 200},
-    """ {
-        "name": "toothpaste",
-        "keeps_days": 365,
-        "buy": 1.80,
-        "sell": 2.30,
-        "order_size": 100,
-    },
-    {"name": "carrot", "keeps_days": 14, "buy": 0.4, "sell": 1, "order_size": 200},
-    {"name": "avocado", "keeps_days": 6, "buy": 1, "sell": 1.20, "order_size": 150},
-    {"name": "toilet paper", "keeps_days": 365, "buy": 2, "sell": 4, "order_size": 100},
-    {
-        "name": "salmon",
-        "keeps_days": 4,
-        "buy": 1.7,
-        "sell": 4,
-        "last_day_discount": True,
-        "order_size": 50,
-    },
-    {
-        "name": "quinoa salad",
-        "keeps_days": 4,
-        "buy": 2.3,
-        "sell": 4.5,
-        "last_day_discount": True,
-        "order_size": 50,
-    },
-    {
-        "name": "frozen pizza",
-        "keeps_days": 150,
-        "buy": 0.7,
-        "sell": 2.5,
-        "order_size": 100,
-    },
-    {"name": "cola", "keeps_days": 365, "buy": 0.7, "sell": 2.5, "order_size": 200},
-    {
-        "name": "croissant",
-        "keeps_days": 1,
-        "buy": 0.7,
-        "sell": 1,
-        "last_hour_discount": True,
-        "order_size": 50,
-    },
-    {
-        "name": "bread",
-        "keeps_days": 1,
-        "buy": 0.4,
-        "sell": 1.2,
-        "last_hour_discount": True,
-        "order_size": 50,
-    }, """,
-]
+class Hours:
+    def __init__(self, opening, closing, delivery):
+        self.opening = opening
+        self.closing = closing
+        self.delivery = delivery
 
-# test
+
+class Product:
+    def __init__(self, product):
+        self.product = product
+
+    def get_product_name(self):
+        return self.product["name"]
+
+    def get_product_value_by_keyname(self, key):
+        return self.product[key]
+
+    def amount_sold_generator(self, from_percentage, till_percentage):
+        return
+
+
+class Reports:
+    def __init__(self, product, sales, orders):
+        self.product = product
+        self.sales = sales
+        self.orders = orders
+
+    def hourly_report(self, product, sales):
+        return
+
+    def daily_report(self, product, sales):
+        return
+
+    def weekly_report(self, product, sales):
+        return
+
+
+class GenerateTxtReport:
+    def __init__(self):
+        return
+
+
+class OnSale:
+    def __init__(self, product):
+        return
+
+    def random_33_off(self):
+        # if sale item in list date is >= 3 days ago
+        # if is_third_off or is_half_off = False
+        # sale random product from lnt(products) --> is_third_off = True
+        # on_sale_price = amount * 0.66
+        # date_third_off = todaydate
+        return
+
+    def random_50_off(self):
+        # if date is 3 days ago
+        # if is_third_off or is_half_off  = False
+        # sale random product from lnt(products) --> is_half_off = True
+        # on_sale_price = amount * 0.50
+        # date_half_off = todaydate
+        return
+
+    def almost_expired_sale(self):
+        # if date is 1 days from today
+        # expire_date_sale = True
+        # date_half_off = todaydate
+        # on_sale_price = (amount * 0.65) or (on_sale_price * 0.65)
+        return
+
+
+class Order:
+    def __init__(self):
+        return
+
+
+albert_van_den_broek = Hours(8, 22, 12)
+start_date = datetime.datetime(2020, 1, 6, 8)
+end_date = datetime.datetime(2020, 1, 7, 8)
+
+
+def report_hourly(start, finish):
+    while finish > start:
+        start = start + datetime.timedelta(hours=1)
+        yield start
+
+
+for hour in report_hourly(start_date, end_date):
+    if hour.hour > 8 and hour.hour < 22:
+        print(f"print {hour.hour} o' clock report")
+    if hour.hour == 12:
+        print(f"it's {hour.hour} o'clock, delivery is here!")
+    if hour.hour == 22:
+        print(f"it's {hour.hour} o'clock, end of day report")
+
+
+""" def random_subtractor_per_hour(subtractable_amount, hours):
+    subtracted = []
+    large_num = subtractable_amount / 2
+    while hours != 0:
+        num = random.randint(0, large_num)
+        if subtractable_amount != 0 or subtractable_amount > num:
+            subtracted.append(num)
+            subtractable_amount = subtractable_amount - num
+            hours -= 1
+        else:
+            subtracted.append(subtractable_amount)
+            subtractable_amount - subtractable_amount
+            hours -= 1
+
+        print(subtracted)
+
+
+random_subtractor_per_hour(100, 12) """
